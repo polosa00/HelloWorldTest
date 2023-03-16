45ab700f086d55cba6e5cd24d14f5f872f43c0bd
polosa00 <i@apolochanin.ru>
2023-03-16 21:44:26 +0300

    Work with VC

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        greetingLabel.isHidden.toggle() // toggle() переворачивает логическое значение наооборот
+        greetingButton.layer.cornerRadius = 10
     }
 
 
+    @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting", 
+            for: .normal
+        )
+        
+       
+    }
 }
 
